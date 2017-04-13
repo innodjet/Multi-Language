cbd4a15d9b5e9689d96561ff505af9652fac9fc3
NIK DJETELI <innodjet@gmail.com>
2017-04-13 12:33:45 -0500

    Update En.php

--- En.php
+++ En.php
@@ -2,7 +2,6 @@
 $lang=array(
     "Hello"=>"Hello",
     "My name is"=>"My name is",
-	"I am a software Engineer"=>"I am a software Engineer"
-	
+	"I am a software Engineer"=>"I am a software Engineer"	
 );
 ?>
